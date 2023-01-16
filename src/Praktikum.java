117f5627956eae596f2351cf33ae0ff8f972d910
ilya-noize <shumakoff.ilya@yandex.ru>
2023-01-16 18:22:30 +0300

    Добавлен функционал меню

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -4,13 +4,22 @@ public class Praktikum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите год:");
-        int year = scanner.nextInt();
 
-        if (isLeapYear(year)) {
-            System.out.println("12.09."+ year);
-        } else {
-            System.out.println("13.09."+year);
+        int command;
+        while(true){
+            printMenu();
+            command = scanner.nextInt();
+            if(command == 1) {
+                System.out.println("▒ Введите год:");
+                int year = scanner.nextInt();
+                if (isLeapYear(year)) {
+                    System.out.println("12.09." + year);
+                } else {
+                    System.out.println("13.09." + year);
+                }
+            } else if (command == 0) {
+                return;
+            }
         }
     }
     public static boolean isLeapYear(int year) {
@@ -19,7 +28,11 @@ public class Praktikum {
         * остальные годы, номер которых кратен 100, — не високосные (например, годы 1700, 1800, 1900, 2100, 2200, 2300);
         * остальные годы, номер которых кратен 4, — високосные.
         */
-        return year % 400 == 0 || year % 4 == 0;
+        return !(year % 400 == 0 || year % 4 == 0);
     }
 
+    public static void printMenu(){
+        System.out.println("▒ 1 - День программиста");
+        System.out.println("▒ 0 - Выход");
+    }
 } 
